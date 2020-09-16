fc0ace346d7f1d0020752431f8d6f055bdec44c3
NikitaPakhomov <NikitaPakhomov@users.noreply.github.com>
2020-09-16 16:59:08 +0700

    2

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let sound = new Audio();
 sound.src = 'music/hover__music.mp3';
-const title = document.querySelector('.title');
-
-
+const title = document.querySelector('.header__h1');
 
 title.addEventListener('mouseover', function () {
     sound.play();
