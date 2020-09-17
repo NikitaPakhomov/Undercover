f53182dd1186d6c96c4452dd35e0a6e124b788ec
NikitaPakhomov <NikitaPakhomov@users.noreply.github.com>
2020-09-17 15:52:38 +0700

    port

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//---------SOUND---------
 let sound = new Audio();
 sound.src = 'music/hover__music.mp3';
 const title = document.querySelector('.header__h1');
@@ -5,3 +6,22 @@ const title = document.querySelector('.header__h1');
 title.addEventListener('mouseover', function () {
     sound.play();
 });
+
+//-----------portfolio-------
+let category = document.querySelector('.portfolio__select');
+let items = document.querySelectorAll(".portfolio__item");
+category.addEventListener('change', () => {
+    items.forEach(function (item) {
+        console.log(item.lastElementChild.lastElementChild);
+        item.classList.add('deactive');
+        if (item.lastElementChild.lastElementChild.textContent == category.value) {
+            item.classList.remove('deactive');
+        } else {
+            if (category.value == 'All') {
+                item.classList.remove('deactive');
+            }
+        }
+    })
+});
+
+
